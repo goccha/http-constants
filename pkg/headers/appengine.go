a4bc6bf95903d0bc58b286fc7f73e895cc0e87f3
ryoji <ryoji.gcp@gmail.com>
2020-02-06 21:35:45 +0900

    add appengine

---
+++ pkg/headers/appengine.go
@@ -0,0 +1,14 @@
+package headers
+
+const (
+	XAppEngineCountry               = "X-AppEngine-Country"
+	XAppEngineRegion                = "X-AppEngine-Region"
+	XAppEngineCity                  = "X-AppEngine-City"
+	XAppEngineCityLatLong           = "X-AppEngine-CityLatLong"
+	XAppEngineCron                  = "X-AppEngine-Cron"
+	XAppEngineInboundAppId          = "X-Appengine-Inbound-Appid"
+	XAppEngineEstimatedCPMUSDollars = "X-AppEngine-Estimated-CPM-US-Dollars"
+	XAppEngineResourceUsage         = "X-AppEngine-Resource-Usage"
+	XAppEngineBlobKey               = "X-AppEngine-BlobKey"
+	XAppEngineBlobRange             = "X-AppEngine-BlobRange"
+)
